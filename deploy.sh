467f9cf89fdd4047d51b9fc55f55ca821b986f5a
Geoff Garbers <geoff@garbers.co.za>
2017-11-07 07:13:53 +0200

    Removed spaces from folds

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-travis_fold start "testing travis_nanoseconds"
+travis_fold start "travis_nanoseconds"
 	ns1=$(travis_nanoseconds)
 	echo "The current nanoseconds is: ${ns1}"
 	ns2=$(travis_nanoseconds)
 	elapsed=$((ns2-ns1))
 	echo "${elapsed}ns have elapsed."
-travis_fold end "testing travis_nanoseconds"
+travis_fold end "travis_nanoseconds"
 
-travis_fold start "timing a deployment"
+travis_fold start "deployment.timing"
 	travis_time_start
 		echo "About to sleep to simulate a deployment."
 		sleep 3
@@ -16,4 +16,4 @@ travis_fold start "timing a deployment"
 		sleep 5
 		echo "Finished deployment."
 	travis_time_finish
-travis_fold end "timing a deployment"
+travis_fold end "deployment.timing"
